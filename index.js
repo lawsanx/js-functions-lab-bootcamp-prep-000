02de8c07b157976240602abcfd9118b13d65d4d7
sailboatlarry@gmail.com <sailboatlarry@gmail.com>
2018-05-04 15:41:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 // write your code below!
 function happyHolidays(){
-  return "Happy holidays!";
+  return `Happy ${holiday}, ${name}!`;
+};
 }
 
 function happyHolidaysTo(name){
   return `Happy holidays, ${name}!`;
+}
+
+function happyHolidaysTo(holiday, name){
+  return `Happy ${holiday}, ${name}!`;
+};
 }
